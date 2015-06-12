98a8aa8f270cff3b877c27d7607c142baa63e678
D. C. Rockwell <dcautomates@gmail.com>
2015-06-12 16:30:24 -0600

    split tasks into individual files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,2 +1,4 @@
+import "./tasks/build-spec.js";
+import "./tasks/build-lib.js";
 import "./tasks/build.js";
 import "./tasks/test.js";
--- tasks/build.js
+++ tasks/build.js
@@ -1,18 +1,3 @@
 import gulp from "gulp";
-import babel from "gulp-babel";
-
-import paths from "../paths.json";
 
 gulp.task("build", ["build-lib", "build-spec"]);
-
-gulp.task("build-lib", () => {
-	return gulp.src(paths.source.lib)
-		.pipe(babel())
-		.pipe(gulp.dest(paths.build.directories.lib));
-});
-
-gulp.task("build-spec", () => {
-	return gulp.src(paths.source.spec)
-		.pipe(babel())
-		.pipe(gulp.dest(paths.build.directories.spec));
-});
