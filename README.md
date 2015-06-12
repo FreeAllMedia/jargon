faf907451ac3c2c4c20a2cd2d66309d872064e37
D. C. Rockwell <dcautomates@gmail.com>
2015-06-12 15:17:10 -0600

    code climate integration

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
+[![Code Climate](https://codeclimate.com/repos/557b3d7de30ba0742500838c/badges/d525182d1790d6589836/gpa.svg)](https://codeclimate.com/repos/557b3d7de30ba0742500838c/feed)
+[![Test Coverage](https://codeclimate.com/repos/557b3d7de30ba0742500838c/badges/d525182d1790d6589836/coverage.svg)](https://codeclimate.com/repos/557b3d7de30ba0742500838c/coverage)
 [![Dependency Status](https://david-dm.org/FreeAllMedia/jargon.png?theme=shields.io)](https://david-dm.org/FreeAllMedia/jargon?theme=shields.io)
 [![Dev Dependency Status](https://david-dm.org/FreeAllMedia/jargon/dev-status.svg)](https://david-dm.org/FreeAllMedia/jargon?theme=shields.io#info=devDependencies)
 [![Build Status](https://travis-ci.org/FreeAllMedia/jargon.png?branch=master)](https://travis-ci.org/FreeAllMedia/jargon)
-[![Dev Build Status](https://travis-ci.org/FreeAllMedia/jargon.png?branch=develop)](https://travis-ci.org/FreeAllMedia/jargon)
 
 # Jargon.js
 
@@ -9,11 +10,73 @@ Change string between case formats and pluralization in a chained interface.
 
 # Installation
 
+## Install NPM Package
+
+```
+npm install jargon
+```
+
+## Import Jargon Into Your File
+
+**es6:**
+
+```
+import jargon from "jargon";
+```
+
+**es5:**
+
+```
+var jargon = require("jargon");
+```
+
 # Usage
+
+These are examples of single transforms on a string. All of the methods below can be chained to create more complex transforms. See [`chaining`](#chaining).
 
 ## Pluralization
 
+```
+jargon("apple").plural.toString(); //apples
+```
+
+## Camel casing
+```
+jargon("apple tree").camel.toString(); //appleTree
+```
+
+## Pascal casing
+```
+jargon("apple_tree").pascal.toString(); //AppleTree
+```
+
+## Snake casing
+```
+jargon("appleTree").snake.toString(); //apple_tree
+```
+
+## Foreign Key casing
+```
+jargon("appleTree").foreignKey.toString(); //apple_tree_id
+```
+
+## Table casing
+```
+jargon("apple tree").table.toString(); //apples
+```
+
+## Chaining
+Every method is chainable so you can combine them. But remember to finalize the chain with "toString();".
+
+```
+jargon("apple_tree").camel.plural.toString(); //appleTrees
+```
+
 # Development
+
+## Style Guide
+
+
 
 ## Public Shared Floobits Workspace
 
--- es6/spec/jargon.spec.js
+++ es6/spec/jargon.spec.js
@@ -1,97 +1,97 @@
-import inflect, {Inflector} from "../lib/inflect.js";
+import jargon, {Inflector} from "../lib/jargon.js";
 
-describe("inflect(string)", () => {
+describe("jargon(string)", () => {
 
 	describe("constructor", () => {
 		it("should return a extended string", () => {
-			(inflect("someString") instanceof Inflector).should.be.true;
+			(jargon("someString") instanceof Inflector).should.be.true;
 		});
 	});
 
 	describe(".snake", () => {
 		it("should return an inflector object in order to allow chaining", () => {
-			(inflect("appleTree").snake instanceof Inflector).should.be.true;
+			(jargon("appleTree").snake instanceof Inflector).should.be.true;
 		});
 
 		it("should provide a way to get a snake cased string from camel cased string", () => {
-			inflect("appleTree").snake.toString().should.equal("apple_tree");
+			jargon("appleTree").snake.toString().should.equal("apple_tree");
 		});
 	});
 
 	describe(".camel", () => {
 		it("should return an inflector object in order to allow chaining", () => {
-			(inflect("appleTree").camel instanceof Inflector).should.be.true;
+			(jargon("appleTree").camel instanceof Inflector).should.be.true;
 		});
 
 		it("should provide a way to get a camel cased string from snake cased string", () => {
-			inflect("apple_tree").camel.toString().should.equal("appleTree");
+			jargon("apple_tree").camel.toString().should.equal("appleTree");
 		});
 
 		it("should get a camel case string from a humanized string (with spaces)", () => {
-			inflect("apple tree").camel.toString().should.equal("appleTree");
+			jargon("apple tree").camel.toString().should.equal("appleTree");
 		});
 
 		it("should get a camel case string from a first-capital humanized string (with spaces)", () => {
-			inflect("Apple tree").camel.toString().should.equal("appleTree");
+			jargon("Apple tree").camel.toString().should.equal("appleTree");
 		});
 	});
 
 	describe(".pascal", () => {
 		it("should return an inflector object in order to allow chaining", () => {
-			(inflect("appleTree").pascal instanceof Inflector).should.be.true;
+			(jargon("appleTree").pascal instanceof Inflector).should.be.true;
 		});
 
 		it("should provide a way to get a camel cased string from snake cased string", () => {
-			inflect("apple_tree").pascal.toString().should.equal("AppleTree");
+			jargon("apple_tree").pascal.toString().should.equal("AppleTree");
 		});
 	});
 
 	describe(".plural", () => {
 		it("should return an inflector object in order to allow chaining", () => {
-			(inflect("appleTree").plural instanceof Inflector).should.be.true;
+			(jargon("appleTree").plural instanceof Inflector).should.be.true;
 		});
 
 		it("should provide a way to get a plural for a string", () => {
-			inflect("apple").plural.toString().should.equal("apples");
+			jargon("apple").plural.toString().should.equal("apples");
 		});
 	});
 
 	describe(".foreignKey", () => {
 		it("should return an inflector object in order to allow chaining", () => {
-			(inflect("appleTree").foreignKey instanceof Inflector).should.be.true;
+			(jargon("appleTree").foreignKey instanceof Inflector).should.be.true;
 		});
 
 		it("should provide a way to get a foreign key id (add _id + underscore) formatted field for a string", () => {
-			inflect("appleTree").foreignKey.toString().should.equal("apple_tree_id");
+			jargon("appleTree").foreignKey.toString().should.equal("apple_tree_id");
 		});
 	});
 
 	describe(".table", () => {
 		it("should return an inflector object in order to allow chaining", () => {
-			(inflect("appleTree").table instanceof Inflector).should.be.true;
+			(jargon("appleTree").table instanceof Inflector).should.be.true;
 		});
 
 		it("should provide a way to get a table name (plural + snake) from a camel cased string", () => {
-			inflect("appleTree").table.toString().should.equal("apple_trees");
+			jargon("appleTree").table.toString().should.equal("apple_trees");
 		});
 	});
 
 	describe("(chaining)", () => {
 		describe(".camel.plural", () => {
 			it("should provide a way to chain camel and plural to", () => {
-				inflect("apple_tree").camel.plural.toString().should.equal("appleTrees");
+				jargon("apple_tree").camel.plural.toString().should.equal("appleTrees");
 			});
 		});
 
 		describe(".plural.snake", () => {
 			it("should provide a way to chain camel and plural to", () => {
-				inflect("appleTree").plural.snake.toString().should.equal("apple_trees");
+				jargon("appleTree").plural.snake.toString().should.equal("apple_trees");
 			});
 		});
 
 		describe(".foreignKey.camel", () => {
 			it("should provide a way to chain camel and plural to", () => {
-				inflect("appleTree").foreignKey.camel.toString().should.equal("appleTreeId");
+				jargon("appleTree").foreignKey.camel.toString().should.equal("appleTreeId");
 			});
 		});
 	});
