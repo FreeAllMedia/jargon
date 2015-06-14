fb1c895850c2a6cf39d8124943c8edeb0f47527f
D. C. Rockwell <dcautomates@gmail.com>
2015-06-14 17:25:43 -0600

    Turning off sauce connect

--- .karma.conf.js
+++ .karma.conf.js
@@ -108,7 +108,6 @@ module.exports = function(config) {
     // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_DEBUG,
