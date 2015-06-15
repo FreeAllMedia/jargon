028e1c62c47f17028dabbdbe01f5fc51e9b6faaf
D. C. Rockwell <dcautomates@gmail.com>
2015-06-14 18:44:16 -0600

    Trying a longer timeout

--- .karma.conf.js
+++ .karma.conf.js
@@ -118,6 +118,7 @@ module.exports = function(config) {
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: Object.keys(customLaunchers), // "Chrome", "Firefox", "Safari", "Opera", "IE"],
 
+    captureTimeout: 120000,
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
