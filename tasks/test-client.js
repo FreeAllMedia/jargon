40f136b5a4e75aefd0fe2bd22fa64beed94cadde
D. C. Rockwell <dcautomates@gmail.com>
2015-06-14 21:40:32 -0600

    This hopefully causes karma to only run on the first travis job

--- tasks/test-client.js
+++ tasks/test-client.js
@@ -2,8 +2,18 @@ var gulp = require("gulp");
 var karma = require("karma").server;
 
 gulp.task("test-client", function (done) {
-  karma.start({
-    configFile: __dirname + "/../.karma.conf.js",
-    singleRun: true
-  }, done);
+	if (process.env.TRAVIS_BUILD_NUMBER) {
+		if (process.env.TRAVIS_JOB_NUMBER === `${process.env.TRAVIS_BUILD_NUMBER}.1`) {
+			runKarma(done);
+		}
+	} else {
+		runKarma(done);
+	}
 });
+
+function runKarma(done) {
+	karma.start({
+		configFile: __dirname + "/../.karma.conf.js",
+		singleRun: true
+	}, done);
+}
