5cb3416354566fb8d9568c6e906bbc6d3292bcf3
D. C. Rockwell <dcautomates@gmail.com>
2015-06-14 23:13:52 -0600

    Something is off with the browser tests

--- .karma.conf.js
+++ .karma.conf.js
@@ -87,7 +87,7 @@ if (process.env.SAUCE_USERNAME) {
 module.exports = function(config) {
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    configOptions.logLevel = config.LOG_INFO;
+    configOptions.logLevel = config.LOG_DEBUG;
 
     config.set(configOptions);
 };
